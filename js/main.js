8026694c177b72cb4c6040eb07eae4f7334606ce
HossaamHassan <hossam.h.eldeeb@gmail.com>
2023-08-07 03:15:41 +0300

    Modified Some Files

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ function showSlides(n) {
     dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    dots[slideIndex-1].className += "active";
     captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
@@ -52,3 +52,4 @@ function myFunction() {
       }
     }
   }
+
